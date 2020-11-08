b2e5cee79d5a36f606ebcf29ca30591ad4c7a3eb
mihaly <meszaros@mihaly.me>
2020-11-08 19:46:33 +0100

    Add project files.

---
+++ KdlCore.Native.Commons/Wrappers/Kernel32.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace KdlCore.Native.Commons.Wrappers
+{
+    internal class Kernel32
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool SetDllDirectory(string lpPathName);
+    }
+}
---
+++ KdlCore.Native.Commons/Wrappers/NtDll.cs
@@ -0,0 +1,18 @@
+﻿using KdlCore.Native.Commons.Win32Types;
+using System;
+using System.Runtime.InteropServices;
+
+namespace KdlCore.Native.Commons.Wrappers
+{
+    internal class NtDll
+    {
+        [DllImport("ntdll.dll")]
+        internal static extern NtStatus NtQuerySystemInformation(
+            [In] SystemInformationClass systemInformationClass,
+            IntPtr systemInformation,
+            [In] int systemInformationLength,
+            // ReSharper disable once OptionalParameterRefOut
+            [Out][Optional] out int returnLength
+        );
+    }
+}
